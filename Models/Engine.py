c36b4674faef928788a41d585686387dba198612
Ryan Song <ryansong612@gmail.com>
2023-10-12 20:31:26 +0100

    Stylized

--- Models/Engine.py
+++ Models/Engine.py
@@ -2,12 +2,12 @@
 This is a package that gives user direct access to popular functions.
 '''
 
-
 import tensorflow_hub as hub
 import tensorflow as tf
 import numpy as np
 
-def EmbedSentences(sentences):
+
+def embed_sentences(sentences):
     # Load the Universal Sentence Encoder module
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
     model = hub.load(module_url)
@@ -15,7 +15,8 @@ def EmbedSentences(sentences):
     embeddings = model(sentences)
     return embeddings
 
-def Similarity(embedding1, embedding2):
+
+def similarity(embedding1, embedding2):
     # Compute cosine similarity between two embeddings
-    similarity = np.inner(embedding1, embedding2)
-    return similarity
+    sim = np.inner(embedding1, embedding2)
+    return sim
